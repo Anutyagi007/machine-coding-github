ba093d473e503b7796a76b69e7421c0b9903f1cc
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2026-03-24 23:57:23 +0530

    otp initial

--- src/components/OTP/OTP.jsx
+++ src/components/OTP/OTP.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const OTP = () => {
+  const otpLength = 6;
+  const [otp, setOtp] = useState(Array(otpLength).fill(''));
   return (
-    <div>OTP</div>
+    <div>
+      <h2>OTP Component</h2>
+      {
+        otp.map((_, index) => (
+          <input 
+            key={index} 
+            type="text"
+          />
+        ))
+
+      }
+    </div>
   )
 }
 
