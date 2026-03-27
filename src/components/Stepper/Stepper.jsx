b69985ec20b129b32113b577f911c2a9aebe97a8
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2026-03-28 03:36:33 +0530

    stepper updates

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
+import React from "react";
+import "./Stepper.css";
 
 const Stepper = () => {
-    const steps = [
+  const steps = [
     {
-      label: 'Personal Info',
-      content: <div>Personal Info Content</div>
+      label: "Personal Info",
+      content: <div>Personal Info Content</div>,
     },
     {
-      label: 'Account Info',
-      content: <div>Account Info Content</div>
+      label: "Account Info",
+      content: <div>Account Info Content</div>,
     },
     {
-      label: 'Payment',
-      content: <div>Payment Content</div>
+      label: "Payment",
+      content: <div>Payment Content</div>,
     },
     {
-      label: 'Confirmation',
-      content: <div>Confirmation Content</div>
+      label: "Confirmation",
+      content: <div>Confirmation Content</div>,
     },
     {
-      label: 'Review',
-      content: <div>Review Content</div>
-    }
-  ]
+      label: "Review",
+      content: <div>Review Content</div>,
+    },
+  ];
   return (
-    <div>Stepper</div>
-  )
-}
+    <div>
+      <h2>Stepper Component</h2>
+      <div>
+        {steps.map((step, index) => {
+          return (
+            <div style={{display: 'flex'}}>
+              <div style={{display: 'flex', flexDirection: 'column'}}>
+                <span className="step_number">{index + 1}</span>
+                <span className="step_line"></span>
+              </div>
+              <span>{step.label}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Stepper
+export default Stepper;
