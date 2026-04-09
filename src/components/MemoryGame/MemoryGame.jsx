ffdeb76e936ef0b8fac96b88b66b6430e89c6a48
Anubhav Tyagi <43014815+Anutyagi007@users.noreply.github.com>
2026-04-09 19:49:25 +0530

    memory game complete

--- src/components/MemoryGame/MemoryGame.jsx
+++ src/components/MemoryGame/MemoryGame.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './MemoryGame.css'
-
-function generateGrid() {
-    const arr = Array.from({length: 18}, (_, index) => index+1);
-    const grid = [...arr, ...arr].sort(() => Math.random() - 0.5);
-    const cards = grid.map((item, index) => {
-        return { id: index, number: item, isFlipped: false}
-    });
-    return cards;
-}
 
 const MemoryGame = () => {
     const [grid, setGrid] = useState(generateGrid());
-    console.log(grid);
+    const [isLock, setIsLock] = useState(false);
+    const [flippedCards, setFlippedCards] = useState([]);
     const handleClick = (id) => {
+        if(grid[id].isFlipped  || isLock) {
+            return;
+        }
         const copyCards =[...grid];
         copyCards[id].isFlipped = true;
         setGrid(copyCards);
+        setFlippedCards([...flippedCards, id]);
     }
+    useEffect(()=> {
+        if(flippedCards.length === 2) {
+            setIsLock(true);
+            setTimeout(()=> {
+                if(grid[flippedCards[0]].number !== grid[flippedCards[1]].number) {
+                    setGrid((prevGrid) => {
+                        const copyCards = [...prevGrid];
+                        copyCards[flippedCards[0]].isFlipped = false;
+                        copyCards[flippedCards[1]].isFlipped = false;
+                        return copyCards;
+                    })
+                }
+                setIsLock(false);
+                setFlippedCards([]);
+            }, 3000 )
+        }
+    }, [flippedCards])
   return (
     <div >
         <h2>Memory Game Component</h2>
@@ -34,4 +47,13 @@ const MemoryGame = () => {
   )
 }
 
-export default MemoryGame
+export default MemoryGame;
+
+function generateGrid() {
+    const arr = Array.from({length: 18}, (_, index) => index+1);
+    const grid = [...arr, ...arr].sort(() => Math.random() - 0.5);
+    const cards = grid.map((item, index) => {
+        return { id: index, number: item, isFlipped: false}
+    });
+    return cards;
+}
