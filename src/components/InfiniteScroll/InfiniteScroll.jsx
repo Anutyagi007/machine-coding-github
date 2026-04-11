8c9111d57215f900abf2cb6e3dc02f6222145fc4
Anubhav Tyagi <43014815+Anutyagi007@users.noreply.github.com>
2026-04-11 19:44:49 +0530

    Infinite scroll complete

--- src/components/InfiniteScroll/InfiniteScroll.jsx
+++ src/components/InfiniteScroll/InfiniteScroll.jsx
@@ -1,15 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './InfiniteScroll.css';
 const InfiniteScroll = () => {
     const [photos, setPhotos] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
     const fetchImages = async() => {
-        const data = await fetch('https://picsum.photos/v2/list?page=1&limit=3');
+        const data = await fetch(`https://picsum.photos/v2/list?page=${currentPage}&limit=3`);
         const json = await data.json();
-        setPhotos(json);
+        setPhotos((data) => [...data, ...json]);
     }
     useEffect(()=> {
         fetchImages()
-    },[]);
+    },[currentPage]);
+    useEffect(()=> {
+        const observer = new IntersectionObserver((params)=>{
+            if(params[0]?.isIntersecting) {
+                observer.unobserve(lastImage);
+                setCurrentPage((page)=>page+1);
+            }
+        })
+
+        const lastImage = document.querySelector('.image-post:last-child');
+        console.log(lastImage);
+        if(!lastImage) {
+            return;
+        }
+        observer.observe(lastImage);
+        // return() => {
+        //     if(lastImage) {
+        //         observer.unobserve(lastImage);
+        //     }
+        //     observer.disconnect();
+        // }
+    }, [photos])
     console.log(photos);
   return (
     <div>
@@ -17,7 +39,7 @@ const InfiniteScroll = () => {
         <div className='infinite-image-container'>
             {
                 photos?.map((photo)=> {
-                    return <img key={photo.id} src={photo.download_url} alt={photo.url}/>
+                    return <img className='image-post' key={photo.id} src={photo.download_url} alt={photo.url}/>
                 })
             }
         </div>
