924ecd1f8505ac1b7c6d7b305276c62140083636
Anubhav Tyagi <43014815+Anutyagi007@users.noreply.github.com>
2026-04-13 22:55:30 +0530

    Tabs complete

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import Profile from "./Profile";
+import Interests from "./Interests";
+import Settings from "./Settings";
+import "./Tabs.css";
 
 const Tabs = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const tabs = [
+    {
+      id: 1,
+      name: "Profile",
+      content: Profile,
+    },
+    {
+      id: 2,
+      name: "Interests",
+      content: Interests,
+    },
+    {
+      id: 3,
+      name: "Settings",
+      content: Settings,
+    },
+  ];
+  const ActiveComponent = tabs[currentIndex].content;
   return (
-    <div>Tabs</div>
-  )
-}
+    <div>
+      <div>
+        {tabs.map((tab, index) => {
+          return (
+            <span
+              className="tab-com"
+              style={{ borderBottom: `${currentIndex === index ? '2px solid blue': '2px solid black'}` }}
+              key={tab.idd}
+              onClick={()=>setCurrentIndex(index)}
+            >
+              {tab.name}
+            </span>
+          );
+        })}
+      </div>
+      <div className="active-comp">{<ActiveComponent />}</div>
+    </div>
+  );
+};
 
-export default Tabs
+export default Tabs;
