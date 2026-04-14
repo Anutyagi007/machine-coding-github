44bfcfc6df9897241c4a21dbccc8eb76fb87f568
Anubhav Tyagi <43014815+Anutyagi007@users.noreply.github.com>
2026-04-14 09:46:06 +0530

    Progress Bar complete

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './ProgressBar.css'
 
 const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(()=> {
+      setProgress((prog) => prog < 100 ? prog + 10 : prog);
+    }, 2000);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [progress])
   return (
-    <div>ProgressBar</div>
+    <div >
+        <div className='outer-div' >
+            <div className='inner-div' style={{width: `${progress}%`}}>{progress}%</div>
+        </div>
+        <div>
+          <button onClick={()=>setProgress((prev) => prev > 0 ? prev - 10 : prev)}>-</button>
+          <button onClick={()=>setProgress((prev) => prev < 100 ? prev + 10 : prev)}>+</button>
+        </div>
+    </div>
   )
 }
 
