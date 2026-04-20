893dcf3196aa23038ce0f41d2f0824b0e66416d8
Anubhav Tyagi <43014815+Anutyagi007@users.noreply.github.com>
2026-04-20 10:05:02 +0530

    TrafficLights complete

--- src/components/TrafficLights/TrafficLights.jsx
+++ src/components/TrafficLights/TrafficLights.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './TrafficLights.css';
 
+const config = [
+  {
+    id: 1,
+    color: 'red',
+  },
+  {
+    id:2,
+    color: 'yellow',
+  },
+  {
+    id:3,
+    color: 'green',
+  }
+]
 const TrafficLights = () => {
+  const [currentCircle, setCurrentCircle] = useState(1);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentCircle((prev) => (prev === config.length ? 1 : prev + 1));
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div>TrafficLights</div>
-  )
+    <div>
+      <div className='circle-container'>
+        {config.map((item) => {
+          return (
+            <div
+              className='circle'
+              key={item.id}
+              style={{ backgroundColor: currentCircle === item.id ? item.color : 'gray' }}
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default TrafficLights
