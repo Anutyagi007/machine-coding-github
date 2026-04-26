7edf61dcee86dff19f4eedbaf99091d5eb975737
Anubhav Tyagi <43014815+Anutyagi007@users.noreply.github.com>
2026-04-26 23:57:56 +0530

    DragDrop complete

--- src/components/DragDrop/DragDrop.jsx
+++ src/components/DragDrop/DragDrop.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import './DragDrop.css';
 
 const DragDrop = () => {
@@ -20,10 +20,30 @@ const DragDrop = () => {
     ],
   };
   const [data, setData] = useState(initialData);
+  const draggedItem = useRef();
+  const draggedContainer = useRef();
+  const handleDragStart = (e, item, container) => {
+    e.target.style.opacity= "0.5";
+    draggedItem.current = item;
+    draggedContainer.current = container;
+  }
+  const handleDragEnd =(e) => {
+    e.target.style.opacity = "1";
+  }
+  const handleDrop = (targetContainer) => {
+    const item = draggedItem.current;
+    const sourceContainer = draggedContainer.current;
+    setData((prev) => {
+        const newData = {...prev};
+        newData[sourceContainer] = newData[sourceContainer].filter((i) => i!==item);
+        newData[targetContainer] = [...newData[targetContainer], item];
+        return newData;    
+    })
+  }
   return <div className="drag-drop-container">
     {
-        Object.keys(data).map((key, index) => <div className="box" key={index}>{key}
-            {data[key].map((item, idx) => <div draggable className="drag-drop-item" key={idx}>{item}</div>)}
+        Object.keys(data).map((key, index) => <div onDrop={() =>handleDrop(key)} onDragOver={(e) => e.preventDefault()} className="box" key={index}>{key}
+            {data[key].map((item, idx) => <div onDragStart={(e) =>handleDragStart(e, item, key)} onDragEnd = {handleDragEnd} draggable className="drag-drop-item" key={idx}>{item}</div>)}
         </div>)
     }
   </div>;
